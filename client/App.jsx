0c884fc134af2ebe3e01f87ff68ec3944e5baafd
Ray Sorenson <stngry78@gmail.com>
2022-04-07 08:28:17 -0500

    added stuff to depression

--- client/App.jsx
+++ client/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.jsx";
+import Depression from "./pages/Depression.jsx";
 
 const App = () => {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Home />} />
+                <Route path="/depression" element={<Depression />} />
             </Routes>
         </BrowserRouter>
     )
