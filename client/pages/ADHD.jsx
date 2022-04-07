75d247cb0c95f572dd8d798a17238b590b4c8a27
makomaster2 <82962727+makomaster2@users.noreply.github.com>
2022-04-07 07:57:16 -0500

    Added temp styling

--- client/pages/ADHD.jsx
+++ client/pages/ADHD.jsx
@@ -5,11 +5,23 @@ const ADHDApp = () => {
     return (
         <>
             {/* //Create Message Input */}
-            <textarea />
-            <input type="submit" />
+            <div className="container">
+            <div id="message-display" className="m-3 p-3">
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis mollitia autem atque, quis vitae nisi corrupti, asperiores numquam, tenetur nulla nesciunt repellendus omnis eius quos obcaecati sint expedita id a.</p>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis mollitia autem atque, quis vitae nisi corrupti, asperiores numquam, tenetur nulla nesciunt repellendus omnis eius quos obcaecati sint expedita id a.</p>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis mollitia autem atque, quis vitae nisi corrupti, asperiores numquam, tenetur nulla nesciunt repellendus omnis eius quos obcaecati sint expedita id a.</p>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis mollitia autem atque, quis vitae nisi corrupti, asperiores numquam, tenetur nulla nesciunt repellendus omnis eius quos obcaecati sint expedita id a.</p>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis mollitia autem atque, quis vitae nisi corrupti, asperiores numquam, tenetur nulla nesciunt repellendus omnis eius quos obcaecati sint expedita id a.</p>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis mollitia autem atque, quis vitae nisi corrupti, asperiores numquam, tenetur nulla nesciunt repellendus omnis eius quos obcaecati sint expedita id a.</p>
+            </div>
+            <div id="message-input" className="d-flex justify-content-center w-100">
+                <input type="text" className="col-10" />
+                <input type="submit" className="col-2 mx-1 btn btn-primary" />
+            </div>
+            </div>
             {/* //Create Submit Button */}
-		</>
-	);
+        </>
+    );
 };
 
 export default ADHDApp;
