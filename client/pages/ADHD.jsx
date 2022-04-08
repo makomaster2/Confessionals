a2ff0a384f459207775641d90ec2be17e318b5c5
makomaster2 <82962727+makomaster2@users.noreply.github.com>
2022-04-08 13:29:30 -0500

    Removed comments

--- client/pages/ADHD.jsx
+++ client/pages/ADHD.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import SentTemplate from '../components/SentTemplate.jsx';
 import RecievedTemplate from '../components/RecievedTemplate.jsx';
-// import MessageTemplate from '../components/msgTemplate';
 
 const ADHDApp = () => {
 	const [message, setMessage] = useState([]);
@@ -47,22 +46,3 @@ const ADHDApp = () => {
 	);
 };
 export default ADHDApp;
-
-// <>
-//     {/* //Create Message Input */}
-//     <div className="container">
-//     <div id="message-display" className="p-3">
-//         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis mollitia autem atque, quis vitae nisi corrupti, asperiores numquam, tenetur nulla nesciunt repellendus omnis eius quos obcaecati sint expedita id a.</p>
-//         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis mollitia autem atque, quis vitae nisi corrupti, asperiores numquam, tenetur nulla nesciunt repellendus omnis eius quos obcaecati sint expedita id a.</p>
-//         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis mollitia autem atque, quis vitae nisi corrupti, asperiores numquam, tenetur nulla nesciunt repellendus omnis eius quos obcaecati sint expedita id a.</p>
-//         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis mollitia autem atque, quis vitae nisi corrupti, asperiores numquam, tenetur nulla nesciunt repellendus omnis eius quos obcaecati sint expedita id a.</p>
-//         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis mollitia autem atque, quis vitae nisi corrupti, asperiores numquam, tenetur nulla nesciunt repellendus omnis eius quos obcaecati sint expedita id a.</p>
-//         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis mollitia autem atque, quis vitae nisi corrupti, asperiores numquam, tenetur nulla nesciunt repellendus omnis eius quos obcaecati sint expedita id a.</p>
-//     </div>
-//     <div id="message-input" className="d-flex justify-content-center w-100">
-//         <input type="text" className="col-10" />
-//         <input type="submit" className="col-2 mx-1 btn btn-primary" />
-//     </div>
-//     </div>
-//     {/* //Create Submit Button */}
-// </>
